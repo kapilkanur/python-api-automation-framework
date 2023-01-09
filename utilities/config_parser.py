9955de0d4238e8103890f96fc0e18823010476e1
Kapil Kanur <kanurkapil@gmail.com>
2023-01-09 11:12:50 +0530

    Updated config parser

--- utilities/config_parser.py
+++ utilities/config_parser.py
@@ -1,6 +1,19 @@
 import yaml
 from yaml.loader import SafeLoader
 
-with open('../config.yaml') as f:
-    data = yaml.load(f, Loader=SafeLoader)
-    print(data)
+try:
+    with open('../config.yaml') as f:
+        try:
+            cfg = yaml.load(f, Loader=SafeLoader)
+            print(cfg)
+        except yaml.YAMLError as exc:
+            print(exc)
+            print("Unable to load the YAML file")
+except FileNotFoundError as exc:
+    print(exc)
+    print("The configuration yaml file does not exist")
+
+
+def get_base_uri():
+    print("The base URI is : " + cfg['base_uri'])
+    return cfg['base_uri']
